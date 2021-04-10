51bbcd837e7987638d2efbd433e9a04f41304583
Josh Woodworth <72992058+josh-woodworth@users.noreply.github.com>
2021-04-09 19:51:09 -0600

    commit tree 35e1b6c7dbe984a5513c7f74fc7d9d59ed5cb2a0

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do 
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Josh"
+  end
+
+  get '/hometown' do
+    "My hometown is Colorado Springs"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Good Song"
+  end
+
 end
